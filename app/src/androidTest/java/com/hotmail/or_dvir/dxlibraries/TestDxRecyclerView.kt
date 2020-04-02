82131e964a7c5abbe253ec85576a0a55b7a53c85
Or Dvir <or_dvir@hotmail.com>
2020-04-02 23:09:41 +0200

    Update TestDxRecyclerView.kt

--- app/src/androidTest/java/com/hotmail/or_dvir/dxlibraries/TestDxRecyclerView.kt
+++ app/src/androidTest/java/com/hotmail/or_dvir/dxlibraries/TestDxRecyclerView.kt
@@ -5,20 +5,15 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TestDxRecyclerView {
+
     //todo DxVisibilityListener
-    // visibility listener is null - nothing is called
-    // no (inner) listener is set - no listener is called
-    // only first visible is set - only it is called
-    // only first invisible is set - only it is called
-    // only last visible is set - only it is called
-    // only last invisible is set - only it is called
+    // no (inner) listeners are set - no listener is called (does that even make sense?)
+    // all listeners are set - only relevant ones are called (check class documentation)
+    // not all listeners are set - non-set listeners aren't called (does that even make sense?)
 
     //todo DxScrollListener
-    // scroll listener is null - nothing is called
-    // no (inner) listener is set - no listener is called
-    // only up is set - only it is called
-    // only down is set - only it is called
-    // only left is set - only it is called - need to change recycler view orientation!
-    // only right is set - only it is called - need to change recycler view orientation!
+    // no (inner) listeners are set - no listener is called (does that even make sense?)
+    // all listeners are set - only relevant ones are called (check class documentation)
+    // not all listeners are set - non-set listeners aren't called (does that even make sense?)
     // how do i test the sensitivity???????
 }
