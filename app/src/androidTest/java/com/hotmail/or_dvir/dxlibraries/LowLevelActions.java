bdd267616654fe601c735eff6ce5a7330081c2e1
Or Dvir <or_dvir@hotmail.com>
2020-05-02 00:04:18 +0200

    attempting some drag tests

---
+++ app/src/androidTest/java/com/hotmail/or_dvir/dxlibraries/LowLevelActions.java
@@ -0,0 +1,75 @@
+package com.hotmail.or_dvir.dxlibraries;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.GeneralLocation;
+import androidx.test.espresso.action.MotionEvents;
+import androidx.test.espresso.action.Press;
+
+import org.hamcrest.Matcher;
+
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
+
+public class LowLevelActions {
+    static MotionEvent sMotionEventDownHeldView = null;
+
+    public static PressAndHoldAction pressAndHold() {
+        return new PressAndHoldAction();
+    }
+
+    public static ReleaseAction release() {
+        return new ReleaseAction();
+    }
+
+    public static void tearDown() {
+        sMotionEventDownHeldView = null;
+    }
+
+    static class PressAndHoldAction implements ViewAction {
+        @Override
+        public Matcher<View> getConstraints() {
+            return isDisplayingAtLeast(90); // Like GeneralClickAction
+        }
+
+        @Override
+        public String getDescription() {
+            return "Press and hold action";
+        }
+
+        @Override
+        public void perform(final UiController uiController, final View view) {
+            if (sMotionEventDownHeldView != null) {
+                throw new AssertionError("Only one view can be held at a time");
+            }
+
+            float[] precision = Press.FINGER.describePrecision();
+            float[] coords = GeneralLocation.CENTER.calculateCoordinates(view);
+            sMotionEventDownHeldView = MotionEvents.sendDown(uiController, coords, precision).down;
+        }
+    }
+
+    static class ReleaseAction implements ViewAction {
+        @Override
+        public Matcher<View> getConstraints() {
+            return isDisplayingAtLeast(90);  // Like GeneralClickAction
+        }
+
+        @Override
+        public String getDescription() {
+            return "Release action";
+        }
+
+        @Override
+        public void perform(final UiController uiController, final View view) {
+            if (sMotionEventDownHeldView == null) {
+                throw new AssertionError("Before calling release(), you must call pressAndHold() on a view");
+            }
+
+            float[] coords = GeneralLocation.CENTER.calculateCoordinates(view);
+            MotionEvents.sendUp(uiController, sMotionEventDownHeldView, coords);
+        }
+    }
+}
--- app/src/androidTest/java/com/hotmail/or_dvir/dxlibraries/TestFeatureDrag.kt
+++ app/src/androidTest/java/com/hotmail/or_dvir/dxlibraries/TestFeatureDrag.kt
@@ -2,8 +2,12 @@ package com.hotmail.or_dvir.dxlibraries
 
 import android.util.Log
 import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.*
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.hotmail.or_dvir.dxdragandswipe.DxFeatureDrag
@@ -12,6 +16,7 @@ import com.hotmail.or_dvir.dxdragandswipe.onItemMovedListener
 import com.hotmail.or_dvir.dxlibraries.draggable.AdapterDraggable
 import com.hotmail.or_dvir.dxlibraries.draggable.ItemDraggable
 import io.mockk.spyk
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -28,7 +33,7 @@ class TestFeatureDrag {
 
     @Before
     fun before() {
-        mDragEventStart = spyk({ _, _ -> Log.i("aaaaa", "drag start")})
+        mDragEventStart = spyk({ _, _ -> Log.i("aaaaa", "drag start") })
         mDragEventEnd = spyk({ _, _ -> })
         mOnItemMoved = spyk({ draggedView, draggedPosition, targetView, targetPosition -> })
 
@@ -39,6 +44,11 @@ class TestFeatureDrag {
             ItemTouchHelper.UP or ItemTouchHelper.DOWN, //may be overridden later
             true
         )
+    }
+
+    @After
+    fun after() {
+        LowLevelActions.tearDown()
     }
 
     private fun onActivity(task: (act: ActivityMain) -> Unit) =
@@ -61,6 +71,12 @@ class TestFeatureDrag {
 //            actionOnItemAtPosition<ViewHolder>(dragFrom, ViewActions.longClick())
 //        )
 
+        //todo try to combine something to make drag and drop...
+
+        onView(withId(R.id.activityMain_rv)).perform(
+            actionOnItemAtPosition<ViewHolder>(dragFrom, LowLevelActions.pressAndHold())
+        )
+
         onView(withId(R.id.activityMain_rv)).perform(
             GeneralSwipeAction(
                 Swipe.SLOW,
@@ -70,8 +86,7 @@ class TestFeatureDrag {
                     ChildViewCoordinatesProvider(R.id.listItem_tv, GeneralLocation.CENTER)
                 ),
                 RecyclerViewCoordinatesProvider(
-                    dragTo,
-                    CustomGeneralLocation.UNDER_RIGHT
+                    dragTo, GeneralLocation.CENTER
                     //CODE FROM STACK OVERFLOW
 //                    if (dragFrom < dragTo) {
 //                        CustomGeneralLocation.UNDER_RIGHT
