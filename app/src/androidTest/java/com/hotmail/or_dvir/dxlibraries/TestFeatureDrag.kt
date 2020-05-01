df9a1e103448a24c2830fde99c6d27f844c66da1
Or Dvir <or_dvir@hotmail.com>
2020-05-01 11:25:25 +0200

    changed application id

--- app/src/androidTest/java/com/hotmail/or_dvir/dxlibraries/TestFeatureDrag.kt
+++ app/src/androidTest/java/com/hotmail/or_dvir/dxlibraries/TestFeatureDrag.kt
@@ -1,11 +1,9 @@
 package com.hotmail.or_dvir.dxlibraries
 
+import android.util.Log
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.GeneralLocation
-import androidx.test.espresso.action.GeneralSwipeAction
-import androidx.test.espresso.action.Press
-import androidx.test.espresso.action.Swipe
+import androidx.test.espresso.action.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.hotmail.or_dvir.dxdragandswipe.DxFeatureDrag
@@ -30,7 +28,7 @@ class TestFeatureDrag {
 
     @Before
     fun before() {
-        mDragEventStart = spyk({ _, _ -> })
+        mDragEventStart = spyk({ _, _ -> Log.i("aaaaa", "drag start")})
         mDragEventEnd = spyk({ _, _ -> })
         mOnItemMoved = spyk({ draggedView, draggedPosition, targetView, targetPosition -> })
 
@@ -38,7 +36,8 @@ class TestFeatureDrag {
             mDragEventStart,
             mDragEventEnd,
             mOnItemMoved,
-            ItemTouchHelper.UP or ItemTouchHelper.DOWN //may be overridden later
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN, //may be overridden later
+            true
         )
     }
 
@@ -55,20 +54,19 @@ class TestFeatureDrag {
 
 
         //remember that this is a non draggable item!!!
-        val dragFrom = 0
-        val dragTo = 2
+        val dragFrom = 2
+        val dragTo = 5
 
 //        onView(withId(R.id.activityMain_rv)).perform(
 //            actionOnItemAtPosition<ViewHolder>(dragFrom, ViewActions.longClick())
 //        )
-
 
         onView(withId(R.id.activityMain_rv)).perform(
             GeneralSwipeAction(
                 Swipe.SLOW,
                 RecyclerViewCoordinatesProvider(
                     dragFrom,
-                    //lets drag from the text view...
+                    //drag from the text view...
                     ChildViewCoordinatesProvider(R.id.listItem_tv, GeneralLocation.CENTER)
                 ),
                 RecyclerViewCoordinatesProvider(
