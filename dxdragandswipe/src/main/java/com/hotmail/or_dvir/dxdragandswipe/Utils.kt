59b52454feba6a344f4e3175dc26f49f84f45921
Or Dvir <or_dvir@hotmail.com>
2020-05-21 17:09:59 +0200

    renamed listeners

--- dxdragandswipe/src/main/java/com/hotmail/or_dvir/dxdragandswipe/Utils.kt
+++ dxdragandswipe/src/main/java/com/hotmail/or_dvir/dxdragandswipe/Utils.kt
@@ -4,14 +4,14 @@ import android.view.View
 import com.hotmail.or_dvir.dxdragandswipe.swipe.DxSwipeBackground
 
 //region drag
-typealias onDragEventListener = (view: View, adapterPosition: Int) -> Unit
+typealias OnDragEventListener = (view: View, adapterPosition: Int) -> Unit
 
-typealias onItemMovedListener =
+typealias OnItemMovedListener =
             (draggedView: View, draggedPosition: Int, targetView: View, targetPosition: Int) -> Unit
 //endregion
 
 //region swipe
-typealias onSwipeEventListener = (view: View, adapterPosition: Int) -> Unit
+typealias OnSwipeEventListener = (view: View, adapterPosition: Int) -> Unit
 
-typealias onItemSwipedListener = (view: View, adapterPosition: Int, direction: Int) -> Unit
+typealias OnItemSwipedListener = (view: View, adapterPosition: Int, direction: Int) -> Unit
 //endregion
