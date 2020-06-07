cb3f2098b681eb4f426bfdacf8cc87b83b894e91
Or Dvir <or.dvir@pixolus.de>
2020-06-07 14:47:28 +0200

    added documentation for DxCountingIdlingResource

--- dxcountingidlingresource/src/main/java/com/hotmail/or_dvir/dxidlingresource/DxCountingIdlingResource.kt
+++ dxcountingidlingresource/src/main/java/com/hotmail/or_dvir/dxidlingresource/DxCountingIdlingResource.kt
@@ -3,6 +3,10 @@ package com.hotmail.or_dvir.dxidlingresource
 import android.util.Log
 import androidx.test.espresso.idling.CountingIdlingResource
 
+/**
+ * a convenience class that holds a [CountingIdlingResource].
+ * This class is used in DxRecyclerView for automated UI tests
+ */
 open class DxCountingIdlingResource(private val uniqueName: String) {
     companion object {
         val TAG = DxCountingIdlingResource::class.java.simpleName
@@ -13,11 +17,18 @@ open class DxCountingIdlingResource(private val uniqueName: String) {
     @JvmField
     val resource = CountingIdlingResource(uniqueName)
 
+    /**
+     * increments the [CountingIdlingResource] held by this class
+     */
     fun increment(calledFrom: String) {
         Log.i(TAG, "idling resource \"$uniqueName\" incremented from $calledFrom")
         resource.increment()
     }
 
+    /**
+     * decrements the [CountingIdlingResource] held by this class. If the resource is idle,
+     * this function does nothing
+     */
     fun decrement(calledFrom: String) {
         if (!resource.isIdleNow) {
             Log.i(TAG, "idling resource \"$uniqueName\" decremented from $calledFrom")
